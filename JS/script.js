79ebe882b18b28d05f10015d572b5a7e9acf80e4
José Daniel Ramos Barquero <josedanielramosbarquero@gmail.com>
2020-09-27 17:58:05 -0600

    Agrego estilos

--- JS/script.js
+++ JS/script.js
@@ -1,24 +1,56 @@
-var modal = document.getElementById("modal");
+//Nissa
 
-// Get the button that opens the modal
-var btn = document.getElementById("nissab");
+//Este proceso llama al div que contiene la ventana
+const ventanaModal = document.getElementById("modal1");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("cerrar")[0];
+//Este proceso llama al boton para abrir el modal del peronaje Nissa.
+const boton = document.getElementById("nissab");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+//cierra la ventana de Nissa.
+const cierre = document.getElementsByClassName("cerrar")[0];
+
+//Muesta el Modal
+boton.onclick = function() {
+  ventanaModal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+//cierra el modal cuando se toca el boton de cierre.
+cierre.onclick = function() {
+  ventanaModal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+//Cierra el modal cuando se toca fuera de dicha ventana.
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  if (event.target == ventanaModal2) {
+    ventanaModal2.style.display = "none";
+  }
+}
+
+
+//Thresh
+
+//Este proceso llama al div que contiene la ventana
+const ventanaModal2 = document.getElementById("modal2");
+
+//Este proceso llama al boton para abrir el modal del peronaje thresh.
+const boton2 = document.getElementById("threshb");
+
+//cierra la ventana de Thresh.
+const cierre2 = document.getElementsByClassName("salir")[0];
+
+//Muesta el Modal
+boton2.onclick = function() {
+  ventanaModal2.style.display = "block";
+}
+
+//cierra el modal cuando se toca el boton de cierre.
+cierre2.onclick = function() {
+  ventanaModal2.style.display = "none";
+}
+
+//Cierra el modal cuando se toca fuera de dicha ventana.
+window.onclick = function(event) {
+  if (event.target == ventanaModal2) {
+    ventanaModal2.style.display = "none";
   }
 }
